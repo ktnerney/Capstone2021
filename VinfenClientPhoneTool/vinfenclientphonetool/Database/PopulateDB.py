2a6e6b387ae3f5e8db44d820ef1ef81c3490205a
tchamp1912 <thomascampion@outlook.com>
2021-03-16 21:37:08 -0400

    Remove unnecessary Camera directory and source files

---
+++ VinfenClientPhoneTool/vinfenclientphonetool/Database/PopulateDB.py
@@ -0,0 +1,52 @@
+import mysql.connector
+import pandas as pd
+
+# Connect to server
+cnx = mysql.connector.connect(
+    host="127.0.0.1",
+    port=3306,
+    user="root",
+    password="")
+
+# Get a cursor
+cursor = cnx.cursor()
+
+# Create Database
+## creating a databse called 'datacamp'
+## 'execute()' method is used to compile a 'SQL' statement
+## below statement is used to create tha 'datacamp' database
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+# Creating Tables
+
+TABLES = {}
+TABLES['employees'] = (
+    "CREATE TABLE `employees` ("
+    "  `emp_no` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `birth_date` date NOT NULL,"
+    "  `first_name` varchar(14) NOT NULL,"
+    "  `last_name` varchar(16) NOT NULL,"
+    "  `gender` enum('M','F') NOT NULL,"
+    "  `hire_date` date NOT NULL,"
+    "  PRIMARY KEY (`emp_no`)"
+    ") ENGINE=InnoDB")
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        cnx.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+# Close connection
+cnx.close()
